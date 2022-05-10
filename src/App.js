a79db605e0be28225cfc73cc844685d5177da8ca
VeronicaTaucci <taucci.veronica@gmail.com>
2022-05-10 12:55:46 -0400

    dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './reducers/index.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
   return (
     <>
-      <p>
-        App component
-      </p>
+      App component
     </>
   );
 }
